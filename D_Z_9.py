51d30d1ae93c30c76feb2519a6a1d62d18616b69
davitmeshveliani <davitmeshveliani979@gmail.com>
2026-02-09 12:59:17 +0100

    add file

---
+++ D_Z_9.py
@@ -0,0 +1,28 @@
+## 1 #############
+
+# n = int(input("Введите число: "))
+#
+# for i in range(1, n + 1):
+#     for nun in range(1, n + 1):
+#         print(i * nun, end="\t")
+#     print()
+
+###  2 ########
+
+# n = int(input("Введите число: "))
+#
+# for i in range(1, n + 1):
+#     for nun in range(1, i + 1):
+#         print(nun, end=" ")
+#     print()
+
+###   3 ######
+
+# text = input("Введите строку: ")
+# result = " "
+#
+# for char in text:
+#     if char == " " or char not in result:
+#         result += char
+#
+# print("Результат:", result )
