bf9970ee104abc853eb4458c0c6ef31dd499a440
davitmeshveliani <davitmeshveliani979@gmail.com>
2026-02-09 22:52:53 +0100

    Восстановил файл davit.fail.txt.py

---
+++ davit.fail.txt.py
@@ -0,0 +1,30 @@
+## 1 #############
+
+# n = int(input("Введите число: "))
+
+# for i in range(1, n + 1):
+#     for nun in range(1, n + 1):
+#         print(i * nun, end="\t")
+#     print()
+
+###  2 ########
+
+# n = int(input("Введите число: "))
+#
+# for i in range(1, n + 1):
+#     for nun in range(1, i + 1):
+#         print(nun, end=" ")
+#     print()
+
+###   3 ######
+
+# text = input("Введите строку: ")
+# result = ""
+#
+# for char in text:
+#     if char not in result:
+#         result += char
+#
+# print("Результат:", result,end=" ")
+
+
